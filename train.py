a1c466ab91d6054fe2c7f7198fa9ab235d13f2be
henok3878 <henokwondimu3878@gmail.com>
2025-07-16 19:15:23 -0400

    train: add training script for DDP, AMP, checkpointing and validation

--- train.py
+++ train.py
@@ -1,6 +1,8 @@
 from functools import partial
+import math
 import os 
 import shutil
+from sentry_sdk import is_initialized
 import wandb 
 from typing import cast
 import torch 
@@ -8,10 +10,11 @@ import argparse
 import torch.nn as nn 
 from dataclasses import asdict 
 from datetime import datetime 
+from torch.optim.lr_scheduler import LambdaLR
 from torch.utils.data import DataLoader, DistributedSampler 
 from torch.nn.parallel import DistributedDataParallel as DDP 
-from torch.distributed import init_process_group, destroy_process_group, broadcast_object_list
-from datasets import load_from_disk 
+import torch.distributed as dist 
+from datasets import Dataset, load_from_disk 
 from tokenizers import Tokenizer 
 
 from config import AppConfig, load_config, TokenizationStrategy 
@@ -21,11 +24,12 @@ def ddp_setup():
     """initializes the distributed process group"""
     local_rank = int(os.environ["LOCAL_RANK"]) 
     torch.cuda.set_device(local_rank)
-    init_process_group(backend='nccl', init_method="env://") 
+    dist.init_process_group(backend='nccl', init_method="env://") 
 
 def ddp_cleanup():
     """clean up distributed process group"""
-    destroy_process_group()
+    if dist.is_initialized():
+        dist.destroy_process_group()
 
 def create_masks(src_batch: torch.Tensor, tgt_batch: torch.Tensor, padding_idx: int) -> tuple[torch.Tensor, torch.Tensor]:
     """
@@ -74,6 +78,7 @@ class Trainer:
     def __init__(self, config: AppConfig, run_path: str):
         self.local_rank = int(os.environ["LOCAL_RANK"])
         self.global_rank = int(os.environ["RANK"])
+        self.world_size = dist.get_world_size()
         self.device = torch.device("cuda", self.local_rank) 
         self.config = config 
         self.run_path = run_path
@@ -83,33 +88,49 @@ class Trainer:
         self.checkpoint_dir = os.path.join(run_path, config.experiment.checkpoint_dir)
         self.log_dir = os.path.join(run_path, config.experiment.log_dir)
 
-        wandb.init(
-            project="transformer-from-scratch",
-            name=os.path.basename(run_path),
-            config=asdict(config),
-            dir=self.log_dir,
-            resume="allow"
-        )
+        if self.global_rank == 0:
+            wandb.init(
+                project="transformer-from-scratch",
+                name=os.path.basename(run_path),
+                config=asdict(config),
+                dir=self.log_dir,
+                resume="allow"
+            )
         
         self._load_tokenizers_and_datasets() 
         
+        torch.set_float32_matmul_precision("high")
         model = Transformer(
             model_config=self.config.model, 
             data_config=self.config.data 
         ).to(self.device) 
+        model = torch.compile(model=model) 
 
         self.model = DDP(model, device_ids=[self.local_rank])
         
         self.optimizer = torch.optim.AdamW(
-            self.model.parameters(), lr=self.config.training.lr
+            self.model.parameters(), lr=self.config.training.peak_lr, weight_decay=config.training.weight_decay, eps=config.training.adam_eps, 
+            fused=True 
         )
+        self.scheduler = LambdaLR(self.optimizer, lr_lambda=self._lr_lambda)
 
-        self.loss_fn = nn.CrossEntropyLoss(ignore_index=self.padding_idx) 
+        self.scaler = torch.GradScaler() 
 
-        self.train_loader = self._prepare_dataloader(self.train_dataset) 
+        self.loss_fn = nn.CrossEntropyLoss(label_smoothing=config.training.label_smoothing, ignore_index=self.padding_idx) 
+
+        self.train_loader = self._prepare_dataloader(self.train_dataset, shuffle=True) 
+        self.full_val_loader = self._prepare_dataloader(dataset=self.full_val_dataset, shuffle=False)
+        self.quick_val_loader = self._prepare_dataloader(self.quick_val_dataset, shuffle=False)
         
-        self.epochs_run = 0
+        self.global_step = 0 
+        self.epochs_run = 0 
         self._load_checkpoint()
+
+    def _lr_lambda(self, step: int):
+        warmup = self.config.training.warmup_steps
+        if step < warmup:
+            return step / warmup 
+        return (step / warmup) ** -0.5 
 
     def _load_tokenizers_and_datasets(self):
         if self.global_rank == 0:
@@ -120,37 +141,42 @@ class Trainer:
         if strategy == TokenizationStrategy.JOINT:
             vocab_size = self.config.model.src_vocab_size
             processed_dir = f"data/processed-joint-{self.config.data.subset}-vocab{vocab_size}"
-            self.train_dataset = load_from_disk(processed_dir)
-            
             tokenizer_path = f"tokenizers/tokenizer-joint-{self.config.data.subset}-vocab{vocab_size}.json"
             self.tokenizer_src = Tokenizer.from_file(tokenizer_path)
             self.tokenizer_tgt = self.tokenizer_src
             
-        elif strategy == TokenizationStrategy.SEPARATE:
+        else: # SEPARATE 
             src_vocab = self.config.model.src_vocab_size
             tgt_vocab = self.config.model.tgt_vocab_size
             processed_dir = f"data/processed-separate-{self.config.data.subset}-src{src_vocab}-tgt{tgt_vocab}"
-            self.train_dataset = load_from_disk(processed_dir)
-
             tokenizer_path_src = f"tokenizers/tokenizer-separate-{self.config.data.lang_src}-vocab{src_vocab}.json"
             tokenizer_path_tgt = f"tokenizers/tokenizer-separate-{self.config.data.lang_tgt}-vocab{tgt_vocab}.json"
             self.tokenizer_src = Tokenizer.from_file(tokenizer_path_src)
             self.tokenizer_tgt = Tokenizer.from_file(tokenizer_path_tgt)
-        else:
-            raise ValueError(f"Invalid tokenization strategy: {strategy}")
+
+        train_dir = os.path.join(processed_dir, "train")
+        val_dir = os.path.join(processed_dir, "validation")
+
+        self.train_dataset: Dataset = load_from_disk(train_dir)  # type: ignore 
+        self.full_val_dataset: Dataset = load_from_disk(val_dir) # type: ignore 
+
+        quick_n = min(len(self.full_val_dataset), self.config.training.quick_val_size) 
+        self.quick_val_dataset = self.full_val_dataset.select(range(quick_n)) 
 
         self.padding_idx = self.tokenizer_src.token_to_id("[PAD]")
     
-    def _prepare_dataloader(self, dataset, is_train=True):
+    def _prepare_dataloader(self, dataset, shuffle: bool):
         collate = partial(prepare_batch, padding_idx=self.padding_idx,
                           src_max_len=self.config.model.src_max_len,
                           tgt_max_len=self.config.model.tgt_max_len)
         return DataLoader(
             dataset, 
             batch_size=self.config.training.batch_size, 
-            sampler=DistributedSampler(dataset, shuffle=is_train), 
+            sampler=DistributedSampler(dataset, shuffle=shuffle, drop_last=shuffle), # only drop the final short batch in training 
+            drop_last=shuffle,
             collate_fn=collate,
             pin_memory=True, 
+            pin_memory_device="cuda",  
             num_workers=self.config.training.num_workers
         ) 
     
@@ -166,6 +192,9 @@ class Trainer:
         checkpoint = torch.load(ckp_path, map_location=map_location)
         self.model.module.load_state_dict(checkpoint["model_state_dict"])
         self.optimizer.load_state_dict(checkpoint["optimizer_state_dict"])
+        self.scheduler.load_state_dict(state_dict=checkpoint['scheduler_state_dict']) 
+        self.scaler.load_state_dict(checkpoint["scaler_state_dict"])
+        self.global_step = checkpoint["global_step"]
         self.epochs_run = checkpoint["epoch"] + 1
         if self.global_rank == 0:
             print(f"Resumed training from epoch {self.epochs_run}")
@@ -183,8 +212,11 @@ class Trainer:
         
         checkpoint = {
             'epoch': epoch,
+            'global_step': self.global_step,
             'model_state_dict': self.model.module.state_dict(),
             'optimizer_state_dict': self.optimizer.state_dict(),
+            "scheduler_state_dict": self.scheduler.state_dict(),
+            "scaler_state_dict": self.scaler.state_dict(),
         }
         
         torch.save(checkpoint, ckp_path)
@@ -217,31 +249,90 @@ class Trainer:
                 os.remove(os.path.join(self.checkpoint_dir, filename))
                 print(f"Removed old checkpoint: {filename}")
     
-    def _should_save_checkpoint(self, epoch: int) -> bool:
-        """Check if we should save a checkpoint this epoch."""
-        return epoch % self.config.experiment.save_every_epoch == 0
+    @torch.inference_mode()
+    def _eval_loader(self, loader: DataLoader): 
+        total_loss = torch.tensor(0.0, device=self.device)
+        total_tokens = torch.tensor(0.0, device=self.device)
+        for batch in loader:
+            batch = {k: v.to(self.device) for k, v in batch.items()}
+            with torch.autocast(device_type="cuda"):
+                logits = self.model(
+                    src_ids=batch["src_ids"],
+                    tgt_ids=batch["tgt_ids"],
+                    src_mask=batch["src_mask"],
+                    tgt_mask=batch["tgt_mask"],
+                )
+                loss = self.loss_fn(logits.reshape(-1, logits.size(-1)), batch["label"].reshape(-1))
+            total_loss += loss * batch["label"].numel()
+            total_tokens += batch["label"].numel()
+
+        # aggregate across processes
+        dist.all_reduce(total_loss, op=dist.ReduceOp.SUM)
+        dist.all_reduce(total_tokens, op=dist.ReduceOp.SUM)
+
+        avg_loss = (total_loss / total_tokens).item()
+        ppl = math.exp(avg_loss)
+        return avg_loss, ppl
     
+    def _run_full_validation(self):
+        avg_loss, ppl = self._eval_loader(loader=self.full_val_loader) 
+        if self.global_rank == 0:
+            wandb.log({"val/full_loss": avg_loss, "val/full_ppl": ppl, "step": self.global_step})
+            print(f"[FULL VAL] step={self.global_step} loss={avg_loss:.4f} ppl={ppl:.2f}")    
+        
+
+    def _run_quick_validation(self): 
+        avg_loss, ppl = self._eval_loader(loader=self.quick_val_loader) 
+        if self.global_rank == 0:
+            wandb.log({"val/quick_loss": avg_loss, "val/quick_ppl": ppl, "step": self.global_step})
+            print(f"[QUICK VAL] step={self.global_step} loss={avg_loss:.4f} ppl={ppl:.2f}")
+
     def _run_batch(self, batch: dict[str, torch.Tensor]) -> float:
         """Runs a single training step."""
         for key, value in batch.items():
             batch[key] = value.to(self.device)
         
-        logits = self.model(
-            src_ids=batch["src_ids"],
-            tgt_ids=batch["tgt_ids"],
-            src_mask=batch["src_mask"],
-            tgt_mask=batch["tgt_mask"]
-        )
+        with torch.autocast(device_type="cuda"): 
+            logits = self.model(
+                src_ids=batch["src_ids"],
+                tgt_ids=batch["tgt_ids"],
+                src_mask=batch["src_mask"],
+                tgt_mask=batch["tgt_mask"]
+            )
 
-        loss = self.loss_fn(
-            logits.reshape(-1, logits.size(-1)), 
-            batch["label"].reshape(-1)
-        )
-        
+            loss = self.loss_fn(
+                logits.reshape(-1, logits.size(-1)), 
+                batch["label"].reshape(-1)
+            )
+            
         self.optimizer.zero_grad()
-        loss.backward()
-        self.optimizer.step()
-        
+        self.scaler.scale(loss).backward()
+        self.scaler.unscale_(optimizer=self.optimizer)
+        torch.nn.utils.clip_grad_norm_(
+            self.model.parameters(), 
+            self.config.training.max_grad_norm
+        )
+        self.scaler.step(optimizer=self.optimizer)
+        self.scaler.update()
+        self.scheduler.step() 
+
+        if self.global_rank == 0:
+            self.global_step += 1
+
+            # logging
+            if self.global_step % self.config.experiment.log_every == 0:
+                wandb.log({"train/loss": loss.item(), "step": self.global_step})
+
+            # quick & full eval
+            if self.global_step % self.config.training.quick_eval_every == 0:
+                self._run_quick_validation()
+            if self.global_step % self.config.training.full_eval_every == 0:
+                self._run_full_validation()
+
+            # checkpoint
+            if self.global_step % self.config.experiment.save_every_steps == 0:
+                self._save_checkpoint(self.global_step)
+
         return loss.item()
 
     def _run_epoch(self, epoch: int):
@@ -261,13 +352,13 @@ class Trainer:
         """Main training loop."""
         for epoch in range(self.epochs_run, self.config.training.epochs):
             self._run_epoch(epoch)
-            
-            if self._should_save_checkpoint(epoch):
+            if self.global_rank == 0:
+                self._run_full_validation()
                 self._save_checkpoint(epoch)
 
 def main():
     parser = argparse.ArgumentParser(description="Transformer Training Script")
-    parser.add_argument("--config", type=str, required=True, help="Path to config file")
+    parser.add_argument("--config", type=str, deafult="./configs/config_de-en.yaml", help="Path to config file")
     args = parser.parse_args()
     config = load_config(args.config)
 
@@ -289,7 +380,7 @@ def main():
     
     # broadcast the run_path to all processes
     object_list = [run_path]
-    broadcast_object_list(object_list, src=0)
+    dist.broadcast_object_list(object_list, src=0)
     run_path = object_list[0]
 
     assert isinstance(run_path, str), "run_path should be a string"
@@ -297,7 +388,8 @@ def main():
         trainer = Trainer(config, run_path)
         trainer.train()
     finally:
-        wandb.finish()
+        if dist.get_rank() == 0:
+            wandb.finish()
         ddp_cleanup()
 
 if __name__ == "__main__":
