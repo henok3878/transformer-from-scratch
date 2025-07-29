9650b82ebe11c692ff4b13c0fdba5b2720c7c96b
henok3878 <henokwondimu3878@gmail.com>
2025-07-29 19:05:50 -0400

    feat(api): reexport main and base classes in __init__.py for cleaner api

---
+++ src/transformer/__init__.py
@@ -0,0 +1,31 @@
+from .transformer import Transformer
+from .components.encoder import Encoder
+from .components.decoder import Decoder
+from .components.encoder_layer import EncoderLayer
+from .components.decoder_layer import DecoderLayer
+from .components.feed_forward import PositionwiseFeedForward
+from .components.input_embedding import InputEmbedding
+from .components.layer_norm import LayerNorm
+from .components.multi_head import MultiHeadAttention
+from .components.positional_encoding import PositionalEncoding
+
+# export base classes for custom extension
+from .components.base.attention import BaseAttention
+from .components.base.feed_forward import BaseFeedForward
+from .components.base.positional_encoding import BasePositionalEncoding
+
+__all__ = [
+    "Transformer",
+    "Encoder",
+    "Decoder",
+    "EncoderLayer",
+    "DecoderLayer",
+    "PositionwiseFeedForward",
+    "InputEmbedding",
+    "LayerNorm",
+    "MultiHeadAttention",
+    "PositionalEncoding",
+    "BaseAttention",
+    "BaseFeedForward",
+    "BasePositionalEncoding",
+]
